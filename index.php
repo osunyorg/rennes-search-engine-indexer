0161f649d7fdcf87d067876fbf94c4c93a61c9bf
Elisabeth <elisabeth.feuillebois@ecedi.fr>
2025-03-12 17:48:46 +0100

    Ref #101458 - Fix files path, API uri, add logs and remove dotenv

--- index.php
+++ index.php
@@ -5,8 +5,5 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$dotenv = new Dotenv();
-$dotenv->load(__DIR__.'/.env');
-
 $index = new Indexation();
 $index->run();
